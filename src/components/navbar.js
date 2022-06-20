e6cfde83a34b6e68978c6fbb24868e20a2d196b7
joanne-lai03 <joanne.lai92@gmail.com>
2022-06-21 00:26:43 +1200

    Add hamburger menu functionality

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BiSearch } from 'react-icons/bi'
 import { BsPerson } from 'react-icons/bs'
 import { HiOutlineMenuAlt4 } from 'react-icons/hi'
@@ -7,8 +7,14 @@ import { FaFacebook, FaInstagram, FaTwitter, FaPinterest, FaYoutube } from 'reac
 import './NavbarStyle.css'
 
 function Navbar() {
+  const [nav, setNav] = useState(false)
+  
+  function handleNav() {
+    setNav(!nav)
+  }
+
   return (
-    <div className="navbar">
+    <div className={nav ? 'navbar navbar-bg' : 'navbar'}>
        <div className="logo">
           <h2>PARADISE</h2>
        </div>
@@ -20,13 +26,14 @@ function Navbar() {
           <li>Views</li>
        </ul>
        <div className="nav-icons">
-          <BiSearch className="icon"/>
+          <BiSearch className="icon" style={{marginRight: '1rem'}}/>
           <BsPerson className="icon"/>
        </div>
-       <div className="hamburger">
+       <div className="hamburger" onClick={handleNav}>
           <HiOutlineMenuAlt4 className="icon" />
        </div>
-       <div className="mobile-menu">
+
+       <div className={nav ? 'mobile-menu active' : 'mobile-menu'}>
          <ul className="mobile-nav">
             <li>Home</li>
             <li>Destinations</li>

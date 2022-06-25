a3202c5c5c337bce841ac6db4c96f89c2a7bafd2
joanne-lai03 <joanne.lai92@gmail.com>
2022-06-25 23:42:31 +1200

    Style promo in search component

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -9,6 +9,7 @@ function Search() {
       <div className="container">
         <div className="left">
           <h2>Luxury resort vacations for two people</h2>
+          <br></br>
           <p>Come experience the very pinnacle of luxury Caribbean vacations for couples at Paradise Resorts. Our all-inclusive beach resorts, set along the most gorgeous tropical settings and exquisite beaches in the Caribbean Islands offer tranquility and stunning views right on your door step. All resorts feature all-day gourmet dining, unique bars serving premium liquors and wine and a variety of water sports including certified scuba diving. If you are planning a wedding, Paradise is the leader in Caribbean destination weddings and honeymoon packages.</p>
         <div className="search-col-2">
           <div className="box">
@@ -31,7 +32,7 @@ function Search() {
         </div>
         <div className="right">
           <div className="promo">
-            <h4 className="save">GET AN ADDITIONAL 10% OFF</h4>
+            <h4 className="save">GET AN ADDITIONAL 5% OFF</h4>
             <p className="timer">12 HOURS LEFT</p>
             <p className="offers">VIEW ALL CURRENT OFFERS</p>
           </div>

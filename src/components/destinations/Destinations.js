10db8b8081278224006468635fee977ce275134f
joanne-lai03 <joanne.lai92@gmail.com>
2022-06-27 00:04:27 +1200

    Add css styling to Views Component

--- src/components/destinations/Destinations.js
+++ src/components/destinations/Destinations.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './DestinationsStyle.css'
 
 import BoraBora from '../../assets/borabora.jpg'
-import BoraBora2 from '../../assets/borabora2.jpg'
+import Antigua from '../../assets/antigua.jpg'
 import Island from '../../assets/island.jpg'
 import Island2 from '../../assets/island2.jpg'
 import Bahamas from '../../assets/bahamas.jpg'
@@ -15,7 +15,7 @@ function Destinations() {
         <h3>On Caribbean's Best Beaches</h3>
         <div className="img-container">
           <img className ='span-3 image-grid-row-2' src={BoraBora} alt="BoraBora" />
-          <img src={BoraBora2} alt="BoraBora2" />
+          <img src={Antigua} alt="Antigua" />
           <img src={Island} alt="Island" />
           <img src={Island2} alt="Island2" />
           <img src={Bahamas} alt="Bahamas" />
--- src/components/views/Views.js
+++ src/components/views/Views.js
@@ -3,7 +3,7 @@ import './ViewsStyle.css'
 
 import Barbados from '../../assets/barbados.jpg'
 import BoraBora from '../../assets/borabora.jpg'
-import BoraBora2 from '../../assets/borabora2.jpg'
+import Antigua from '../../assets/antigua.jpg'
 import Island from '../../assets/island.jpg'
 import Island2 from '../../assets/island2.jpg'
 import Bahamas from '../../assets/bahamas.jpg'
@@ -13,12 +13,14 @@ import ViewsImg from '../ViewsImg/ViewsImg'
 function Views() {
   return (
     <div className="views">
-      <ViewsImg bgImg={BoraBora2} text="Antigua" />
+      <div className="container">
+      <ViewsImg bgImg={Antigua} text="Antigua" />
       <ViewsImg bgImg={Barbados} text="Barbados" />
       <ViewsImg bgImg={BoraBora} text="Bora Bora" />
       <ViewsImg bgImg={Island} text="Trinidad" />
       <ViewsImg bgImg={Island2} text="Grenada" />
       <ViewsImg bgImg={Bahamas} text="Bahamas" />
+      </div>
     </div>
   )
 }

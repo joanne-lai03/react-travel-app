1d273af51d8cc2b077c9bfe01ef0586bb73396ad
joanne-lai03 <joanne.lai92@gmail.com>
2022-06-28 20:18:03 +1200

    Add in carousel images

--- src/components/carousel/ImgCarousel.js
+++ src/components/carousel/ImgCarousel.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import './ImgCarouselStyle.css'
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import { Carousel } from 'react-responsive-carousel'
+
+import Barbados from '../../assets/barbados.jpg'
+import BoraBora from '../../assets/borabora.jpg'
+import Antigua from '../../assets/antigua.jpg'
+import Trinidad from '../../assets/island.jpg'
+import Grenada from '../../assets/island2.jpg'
 
 function ImgCarousel() {
   return (
-    <div>
-      
+    <div className="container">
+      <Carousel className='carousel' showArrows={true} autoPlay={true} infiniteLoop={true}>
+          <div>
+              <img src={Barbados} alt="Barbados" />
+          </div>
+          <div>
+              <img src={BoraBora} alt="Bora Bora" />
+          </div>
+          <div>
+              <img src={Antigua} alt="Antigua" />
+          </div>
+          <div>
+              <img src={Trinidad} alt="Trinidad" />
+          </div>
+          <div>
+              <img src={Grenada} alt="Grenada" />
+          </div>
+      </Carousel>
     </div>
   )
 }
